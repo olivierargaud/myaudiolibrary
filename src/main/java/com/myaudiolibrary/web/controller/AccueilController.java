bb74673112acf0f430f903d10d6c19049702a236
olivierargaud <olivier.argaud@ipilyon.net>
2020-12-02 08:43:22 +0100

    acces a la page d'accueil

--- src/main/java/com/myaudiolibrary/web/controller/AccueilController.java
+++ src/main/java/com/myaudiolibrary/web/controller/AccueilController.java
@@ -2,8 +2,9 @@ package com.myaudiolibrary.web.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -12,6 +13,13 @@ public class AccueilController
 {
 
 
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String accueil(final ModelMap model)
+    {
+
+        return "accueil";
+    }
 
 
 
