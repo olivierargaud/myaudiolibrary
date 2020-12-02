f01c50a3041302f5b0f4ba49adf0ec17ac588254
olivierargaud <olivier.argaud@ipilyon.net>
2020-12-02 09:21:55 +0100

    ajout mapping pour afficher detail artist

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -1,15 +1,49 @@
 package com.myaudiolibrary.web.controller;
 
 
+import com.myaudiolibrary.web.model.Artist;
+import com.myaudiolibrary.web.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 
 @Controller
 @RequestMapping(value = "/artists")
 public class ArtistController
 {
+
+    @Autowired
+    ArtistRepository artistRepository;
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String detailArtist(final ModelMap model , @PathVariable(value = "id") Long id)
+    {
+        Optional<Artist> artistOptional =  artistRepository.findById(id);
+        if (artistOptional.isEmpty())
+        {
+            System.out.println("lancer une exception");
+        }
+
+        Artist artist = artistOptional.get();
+        model.put("emp",artist);
+
+        return "detailArtist";
+
+    }
 
 
 
