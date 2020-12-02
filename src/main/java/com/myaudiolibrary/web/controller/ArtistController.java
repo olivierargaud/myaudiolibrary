63abac43aaa0c4d3f9597158adae1a1d09e7462c
olivierargaud <olivier.argaud@ipilyon.net>
2020-12-02 09:50:07 +0100

    ajout mapping pour afficher la liste des artists et creation fragment menu

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -4,11 +4,15 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
@@ -45,8 +49,31 @@ public class ArtistController
 
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @RequestMapping(method = RequestMethod.GET, value = "",params = {"page","size"})
+    public String listeArtists
+            (
+                    final ModelMap model ,
+                    @RequestParam(defaultValue = "0")Integer page,
+                    @RequestParam (defaultValue = "10")Integer size,
+                    @RequestParam (defaultValue = "ASC")String sortProperty,
+                    @RequestParam (defaultValue = "name")String sortDirection
+            )
+    {
 
 
+        PageRequest pageRequest = PageRequest.of(page,size, Sort.Direction.fromString(sortDirection),sortProperty);
+
+        Page<Artist> artistPage = artistRepository.findAll(pageRequest);
+
+        model.put("listeArtists",artistPage);
+
+
+        return "listeArtists";
+    }
 
 
 }
